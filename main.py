229519d1778e5ff0fb1afd8aeea56b3a6b43f480
Caravestal <58850165+Caravestal@users.noreply.github.com>
2021-02-25 17:00:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,42 @@ import sys as system
 print(system.version)
 a = 2
 print(a)
+
+# a = 4
+# b = 5.6
+# c = 2 + 3j
+# print(a)
+# print(b)
+# print(c)
+# print(type(a))
+#
+# a = 'inzynieria \nsystemow \ninformatycznych'
+# print(a)
+#
+# from math import *
+# a = 10
+# b = 5
+# c = 2
+# dodawanie = a + b
+# mnozenie = a * b
+# dzielenie = a/b
+# dzielenie_calkowite = b // c
+# potega = c ** b
+# print(dodawanie,mnozenie,dzielenie,dzielenie_calkowite,potega)
+# a *= c
+# print(a)
+# print(pow(8,2))
+# print(sqrt(9))
+# #sqrt to pierwiastek 2 stopnia a pow to potęga
+# print(pi)
+#
+# print(u'inżynieria systemów informatycznych')
+# print('wynik dzialania jest rowny a = %(zm)a' % {'zm': 12})
+# napis = 'inzynieria systemow informatycznych'
+# grupa = 2
+# print('specjalizacja %s, grupa %d' %{napis, grupa})
+
+a = 5
+b = 3
+z = 5 - 3
+print('Wynik dzialania %(z1)d-%(z2)d=%(z3)d' %{'z1': a, 'z2': b, 'z3': z})
