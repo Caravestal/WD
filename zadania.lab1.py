bbab966650845baec5b0e01efc98007dc8e5b6e6
Caravestal <58850165+Caravestal@users.noreply.github.com>
2021-02-27 00:51:38 +0100

    Create zadania.lab1.py

---
+++ zadania.lab1.py
@@ -0,0 +1,70 @@
+# zad.1
+# napis1 = 'Wizualizacja'
+# napis2 = 'Danych'
+# x = 3
+# y = 9
+# a = 1.2
+# b = 8.5
+# m = 1 + 4j
+# n = 3 + 2j
+# print(napis1, napis2, x, y, a, b, m, n)
+#
+# zad.2
+# a = 2
+# b = 6
+# c = 1
+# dod = a + b
+# ode = b - c
+# dziel = b/a
+# mno = a * b
+# print(dod,ode, dziel, mno)
+#
+# zad.3
+# a = 2
+# b = 6
+# c = 1
+# a += c
+# print(a)
+# b -= c
+# print(b)
+# c *= b
+# print(c)
+# b /= c
+# print(b)
+# a **= c
+# print(a)
+# a %= c
+# print(a)
+#
+# zad.4
+# from math import *
+# print(exp(10))
+# print(pow(log(5+sin(8) ** 2), 1.0/6.0))
+# print(fmod(3, 55))
+# print(comb(4,80))
+#
+# zad.5
+# imie = 'MARTA'
+# nazwisko = 'HORABIK'
+# print(imie.capitalize(), nazwisko.capitalize())
+#
+# zad.6
+# piosenka = 'la la la la'
+# sub = 'la'
+# print(piosenka.count(sub))
+#
+# zad.7
+# nap = 'element'
+# print(nap[1],nap[6])
+#
+# zad.8
+# piosenka = 'la la la la'
+# print(piosenka.split(' ', 1))
+# print(piosenka.split(' ', 2))
+# print(piosenka.split(' ', 3))
+#
+# zad.9
+# str = 'napis'
+# fl = 7.8
+# sz = hex(438)
+# print(str, fl, sz)
