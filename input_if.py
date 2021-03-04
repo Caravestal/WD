e7daf437e66c05d730050556801f8ee5dcfd09d9
Caravestal <58850165+Caravestal@users.noreply.github.com>
2021-03-04 16:18:57 +0100

    Create input_if.py

---
+++ input_if.py
@@ -0,0 +1,48 @@
+# liczba = input('wprowadź litere: ')
+# print(liczba)
+# print(type(liczba))
+# liczba = float(liczba)
+# print(liczba)
+#
+# import sys as system
+#
+# system.stdout.write("wprowadz lietere")
+# litera = system.stdin.readline()
+# system.stdout.write(litera)
+#
+# wzór na if
+# if warunek:
+#     instrukcje
+# elif warunek1:
+#     instrukcje
+#
+# else:
+#     instrukcje
+# 
+# a = input('wprowadz liczbe a: ')
+# b = input('wprowadz liczbe b: ')
+# a = int(a)
+# b = int(b)
+# # if a > b:
+# #     print('liczba ' + str(a) + ' jest wieksza')
+# # elif a < b:
+# #     print('liczba ' + str (b) + ' jest wieksza')
+# # else
+# #     print('liczby sa rowne')
+# if a == b:
+#     print('liczby sa rowne')
+# else
+#     print('liczby nie sa rowne')
+# 
+# a = input('wprowadz liczbe a: ')
+# b = input('wprowadz liczbe b: ')
+# c = input('wprowadz liczbe c: ')
+# d = input('wprowadz liczbe d: ')
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# d = int(d)
+# if (a > b) & (c > d)
+#     print('liczba a jest wieksza od b i c od d')
+# else
+#     print('liczba a jest mniejsza od b lub c od d')
